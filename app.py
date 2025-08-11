8ca2dc5be92001c8365ab3d241c2efdf9b812b2d
josefalconi <jfalconi@ucsd.edu>
2025-08-11 16:49:50 -0500

    changed Procfile

--- app.py
+++ app.py
@@ -184,6 +184,6 @@ if __name__ == '__main__':
     )
     atexit.register(lambda: scheduler.shutdown())
 
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
  
